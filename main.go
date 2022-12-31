12c1c0df84552336551c171e6f06ec742ac47c88
Igor Michalak <git@igormichalak.com>
2022-12-31 16:29:50 +0100

    concurrent scanning

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
+	"sync"
+)
+
+var (
+	ErrPortClosedOrFiltered = errors.New("the port is closed or filtered")
 )
 
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-		fmt.Println("Connection successful")
+	var wg sync.WaitGroup
+	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			res := make(chan error)
+			//go scanPort("scanme.nmap.org", j, res)
+			go scanPort("127.0.0.1", j, res)
+			if <-res == nil {
+				fmt.Printf("%d open\n", j)
+			}
+		}(i)
 	}
+	wg.Wait()
+}
+
+func scanPort(host string, port int, out chan error) {
+	addr := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		out <- ErrPortClosedOrFiltered
+		return
+	}
+	conn.Close()
+	out <- nil
 }
