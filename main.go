69370b5acc37bcdbba714140c65904c0908b8109
Igor Michalak <git@igormichalak.com>
2023-01-01 15:22:40 +0100

    scanning using worker pools

--- main.go
+++ main.go
@@ -1,40 +1,64 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net"
-	"sync"
+	"sort"
+
+	flag "github.com/spf13/pflag"
 )
 
-var (
-	ErrPortClosedOrFiltered = errors.New("the port is closed or filtered")
-)
-
-func main() {
-	var wg sync.WaitGroup
-	for i := 1; i <= 1024; i++ {
-		wg.Add(1)
-		go func(j int) {
-			defer wg.Done()
-			res := make(chan error)
-			//go scanPort("scanme.nmap.org", j, res)
-			go scanPort("127.0.0.1", j, res)
-			if <-res == nil {
-				fmt.Printf("%d open\n", j)
-			}
-		}(i)
-	}
-	wg.Wait()
+type config struct {
+	hostname string
+	ports    string
+	workers  int
 }
 
-func scanPort(host string, port int, out chan error) {
-	addr := fmt.Sprintf("%s:%d", host, port)
-	conn, err := net.Dial("tcp", addr)
-	if err != nil {
-		out <- ErrPortClosedOrFiltered
-		return
+func worker(ports <-chan int, results chan<- int) {
+	for p := range ports {
+		addr := fmt.Sprintf("scanme.nmap.org:%d", p)
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			results <- 0
+			continue
+		}
+		conn.Close()
+		results <- p
 	}
-	conn.Close()
-	out <- nil
+}
+
+func main() {
+	var cfg config
+	flag.StringVarP(&cfg.hostname, "hostname", "h", "", "e.g. scanme.nmap.org")
+	flag.StringVarP(&cfg.ports, "ports", "p", "", "e.g. 1-1024")
+	flag.IntVarP(&cfg.workers, "workers", "w", 100, "number of workers in the worker pool")
+	flag.Parse()
+
+	ports := make(chan int, 100)
+	results := make(chan int)
+	var openports []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results)
+	}
+
+	go func() {
+		for i := 1; i <= 1024; i++ {
+			ports <- i
+		}
+	}()
+
+	for i := 0; i < 1024; i++ {
+		port := <-results
+		if port != 0 {
+			openports = append(openports, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+	sort.Ints(openports)
+	for _, port := range openports {
+		fmt.Printf("%d open\n", port)
+	}
 }
